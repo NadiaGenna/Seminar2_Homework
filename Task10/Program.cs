9b861dcd2fef795e1a68a7df80e831ee7627e93f
«Nadezhda <nadezhdagenna@mail.ru>
2022-08-10 10:43:04 +0300

    Task10 is ready

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -7,4 +7,4 @@
 Console.WriteLine("Введите трёхзначное число: ");
 int Digit3Number = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(Digit3Number);
-Console.WriteLine("Вторая цифра этого числа -> " + stringNumber[1]);
+Console.WriteLine("Вторая цифра этого числа: " + stringNumber[1]);
